99edfd5ee7cc8a204512c0e6cc2ad9c82c7663b3
TSitdikov <Sitdikov@lanit.ru>
2018-02-22 14:47:49 +0400

    Version 1.40. Possibility to get instances of pages in block elements.

--- src/main/java/ru/lanit/at/pages/block_elements/AbstractBlockElement.java
+++ src/main/java/ru/lanit/at/pages/block_elements/AbstractBlockElement.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.Logger;
 import ru.lanit.at.context.Context;
 import ru.lanit.at.make.Make;
 import ru.lanit.at.make.Wait;
+import ru.lanit.at.pages.AbstractPage;
+import ru.lanit.at.pages.PageCatalog;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 
 public class AbstractBlockElement extends HtmlElement {
@@ -13,8 +15,15 @@ public class AbstractBlockElement extends HtmlElement {
     protected Wait wait;
     protected Make make;
 
+    private PageCatalog pageCatalog;
+
     public AbstractBlockElement() {
-        wait = (Wait) Context.getInstance().getBean("wait");
-        make = (Make) Context.getInstance().getBean("make");
+        wait = Context.getInstance().getBean(Wait.class);
+        make = Context.getInstance().getBean(Make.class);
+        pageCatalog = Context.getInstance().getBean(PageCatalog.class);
+    }
+
+    protected <T extends AbstractPage> T getPage(Class<T> clazz){
+        return pageCatalog.getPage(clazz);
     }
 }
