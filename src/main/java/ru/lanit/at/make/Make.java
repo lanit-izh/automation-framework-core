d8e27989f127f58fda15bd6ac76c86a5965d6812
danilishe <Suetin@lanit.ru>
2018-03-02 19:08:54 +0400

    У класса Wait исправил sec, чтобы можно было задавать дрбные доли секунды, Добавил @Deprecated JS-waiter'am

--- src/main/java/ru/lanit/at/make/Make.java
+++ src/main/java/ru/lanit/at/make/Make.java
@@ -13,16 +13,16 @@ public class Make {
 
     public void clickTo(WebElement elem) {
         wait.untilElementVisible(elem);
-        Boolean iframeFlag = (Boolean) ((JavascriptExecutor)wait.getDriver()).executeScript("return(window == top)");
+        Boolean iframeFlag = (Boolean) ((JavascriptExecutor) wait.getDriver()).executeScript("return(window == top)");
         if (iframeFlag) {
-            ((JavascriptExecutor)wait.getDriver()).executeScript("arguments[0].scrollIntoView(false);window.scrollBy(0, -"+wait.getDriver().manage().window().getSize().height/3+")", elem);
+            ((JavascriptExecutor) wait.getDriver()).executeScript("arguments[0].scrollIntoView(false);window.scrollBy(0, -" + wait.getDriver().manage().window().getSize().height / 3 + ")", elem);
         } else {
-            ((JavascriptExecutor)wait.getDriver()).executeScript("window.scrollTo(0," + (elem.getLocation().getY() - wait.getDriver().manage().window().getSize().height/3) + ")");
+            ((JavascriptExecutor) wait.getDriver()).executeScript("window.scrollTo(0," + (elem.getLocation().getY() - wait.getDriver().manage().window().getSize().height / 3) + ")");
         }
 
         elem.click();
         wait.untilPageLoaded();
-        wait.untilJSComplete();
+//        wait.untilJSComplete();
     }
 
     public void sendKeysTo(WebElement webElement, String message) {
