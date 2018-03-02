c12bfe377590a3f3d46c5f56efa079ead72c5f4c
Рамиль Мухаметдинов <Muhametdinov@lanit.ru>
2018-03-02 11:40:49 +0400

    Изменена операция clickTo. Она теперь пытается промотать страницу на треть, чтоб элемент не был закрыт шапками

--- src/main/java/ru/lanit/at/make/Make.java
+++ src/main/java/ru/lanit/at/make/Make.java
@@ -1,6 +1,7 @@
 package ru.lanit.at.make;
 
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 public class Make {
@@ -12,6 +13,13 @@ public class Make {
 
     public void clickTo(WebElement elem) {
         wait.untilElementVisible(elem);
+        Boolean iframeFlag = (Boolean) ((JavascriptExecutor)wait.getDriver()).executeScript("return(window == top)");
+        if (iframeFlag) {
+            ((JavascriptExecutor)wait.getDriver()).executeScript("arguments[0].scrollIntoView(false);window.scrollBy(0, -"+wait.getDriver().manage().window().getSize().height/3+")", elem);
+        } else {
+            ((JavascriptExecutor)wait.getDriver()).executeScript("window.scrollTo(0," + (elem.getLocation().getY() - wait.getDriver().manage().window().getSize().height/3) + ")");
+        }
+
         elem.click();
         wait.untilPageLoaded();
         wait.untilJSComplete();
