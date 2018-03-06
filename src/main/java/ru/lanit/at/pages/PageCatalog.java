d2f5384de8971f7361605116fe26c5fc69dd3918
TSitdikov <Sitdikov@lanit.ru>
2018-03-06 14:50:06 +0400

    Page catalog javadoc.

--- src/main/java/ru/lanit/at/pages/PageCatalog.java
+++ src/main/java/ru/lanit/at/pages/PageCatalog.java
@@ -10,6 +10,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
+/**
+ * Catalog that initializes, saves and provides PageObjects which are required during the test run.
+ */
 public class PageCatalog {
     private List<AbstractPage> pageList = new LinkedList<>();
     private DriverManager driverManager;
@@ -19,6 +22,12 @@ public class PageCatalog {
      */
     private AbstractPage currentPage;
 
+    /**
+     * Returns instance of requested page object class from catalog. If catalog doesn't have such instance - initializes it and saves in catalog.
+     *
+     * @param clazz Class of page object to find or initialize in catalog.
+     * @return Instance of clazz.
+     */
     public <T extends AbstractPage> T getPage(Class<T> clazz) {
 
         WebDriver actualDriver = driverManager.getDriver();
@@ -45,6 +54,12 @@ public class PageCatalog {
         }
     }
 
+    /**
+     * Returns instance of class if catalog contains such instance or null if it doesn't.
+     *
+     * @param clazz Class of page object that should be found in catalog.
+     * @return Instance of clazz from catalog or null if catalog doesn't have any instance of clazz.
+     */
     @SuppressWarnings("unchecked")
     private <T extends AbstractPage> T getPageFromList(Class<T> clazz) {
         ListIterator<AbstractPage> litr = pageList.listIterator(pageList.size());
@@ -55,14 +70,22 @@ public class PageCatalog {
         return null;
     }
 
+    /**
+     * @return Page object that is currently opened.
+     */
+    public AbstractPage getCurrentPage() {
+        return currentPage;
+    }
+
+    /**
+     * Setup current page.
+     *
+     * @param abstractPage Page object that should be set as current page.
+     */
     public void setCurrentPage(AbstractPage abstractPage) {
         if (currentPage == null || currentPage != abstractPage) {
             currentPage = abstractPage;
         }
-    }
-
-    public AbstractPage getCurrentPage() {
-        return currentPage;
     }
 
     public WebDriver getDriver() {
