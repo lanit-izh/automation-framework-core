5679f2b243eeb770337158800895ab0357731afc
ramil <muhametdinov@lanit.ru>
2018-03-12 07:21:09 +0400

    proxy branch

--- src/main/java/ru/lanit/at/driver/LocalDriverFactory.java
+++ src/main/java/ru/lanit/at/driver/LocalDriverFactory.java
@@ -1,18 +1,22 @@
 package ru.lanit.at.driver;
 
+import net.lightbody.bmp.proxy.ProxyServer;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class LocalDriverFactory {
     private static Logger log = Logger.getLogger(LocalDriverFactory.class);
 
     static WebDriver createInstance(String browserName) {
         WebDriver driver;
+        startProxy(7878);
 
         switch (browserName.toLowerCase()) {
             case "firefox":
@@ -24,11 +28,14 @@ public class LocalDriverFactory {
                 profile.setPreference("layout.spellcheckDefault", 0);
                 profile.setPreference("devtools.selfxss.count", 1500);
                 profile.setPreference("dom.webnotifications.enabled", false);
+                profile.setPreference("network.proxy.socks", "127.0.0.1");
+                profile.setPreference("network.proxy.socks_port", 7878);
                 driver = new FirefoxDriver(profile);
                 break;
 
             case "chrome":
                 ChromeOptions chromeOptions = CapabilitiesManager.getChromeOptions();
+                chromeOptions.addArguments("--proxy-server=http://127.0.0.1:7878");
                 driver = new ChromeDriver(chromeOptions);
                 break;
 
@@ -40,5 +47,15 @@ public class LocalDriverFactory {
         log.info("Создан драйвер для " + browserName);
         return driver;
 
+    }
+
+    private static void startProxy(int port) {
+        ProxyServer bmp = new ProxyServer(port);
+        try {
+            bmp.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        bmp.autoBasicAuthorization("", "mos", "mos");
     }
 }
