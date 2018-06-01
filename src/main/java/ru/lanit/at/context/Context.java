3c12fd3821cc148e2a22bc66ff30070a8137b4de
ramil <Muhametdinov@lanit.ru>
2018-06-01 17:58:22 +0400

    2.46 threadsafety

--- src/main/java/ru/lanit/at/context/Context.java
+++ src/main/java/ru/lanit/at/context/Context.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Context {
-    private static volatile ApplicationContext instance;
+    private static ThreadLocal<ApplicationContext> instance = new ThreadLocal<>();
 
     /**
      * Method to get double check locked, thread safe Spring classpath application context.
@@ -12,13 +12,13 @@ public class Context {
      * @return Instance of Spring {@link ApplicationContext}.
      */
     public static ApplicationContext getInstance() {
-        ApplicationContext localInstance = instance;
+        ApplicationContext localInstance = instance.get();
         if (localInstance == null) {
             synchronized (ApplicationContext.class) {
-                localInstance = instance;
+                localInstance = instance.get();
                 if (localInstance == null) {
-                    instance = localInstance =
-                            new ClassPathXmlApplicationContext("spring-config.xml");
+                    localInstance = new ClassPathXmlApplicationContext("spring-config.xml");
+                    instance.set(localInstance);
                 }
             }
         }
