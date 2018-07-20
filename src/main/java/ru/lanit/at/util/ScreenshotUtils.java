26cdba5f1519d0bd8a844c5712472154eb7d1537
danilishe <Suetin@lanit.ru>
2018-07-20 17:26:01 +0400

    2.52.3: Добавлена обработка алертов при снятии скриншотов

--- src/main/java/ru/lanit/at/util/ScreenshotUtils.java
+++ src/main/java/ru/lanit/at/util/ScreenshotUtils.java
@@ -9,25 +9,56 @@ import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 public class ScreenshotUtils {
 
     @Attachment(value = "Page screenshot", type = "image/png")
     public static byte[] takeScreenshot(WebDriver webDriver) {
-        AShot aShot = new AShot();
-        BufferedImage bufferedImage = aShot.shootingStrategy(
-                ShootingStrategies.viewportPasting(200)
-        ).takeScreenshot(webDriver).getImage();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        if (isAlertPresented(webDriver)) {
+            String alertText = webDriver.switchTo().alert().getText();
+            saveTextLog(alertText, "Alert text");
+            int closeAlertRetries = 0;
+            while (isAlertPresented(webDriver)) {
+                webDriver.switchTo().alert().dismiss();
+                if (closeAlertRetries++ > 10) {
+                    throw new FrameworkRuntimeException("Не удалось сделать скриншот из-за алерта \"" + alertText + "\"");
+                }
+            }
+        }
         try {
+            BufferedImage bufferedImage = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(10))
+                    .takeScreenshot(webDriver)
+                    .getImage();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
             ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
+
             byteArrayOutputStream.flush();
             byte[] imageBytes = byteArrayOutputStream.toByteArray();
             byteArrayOutputStream.close();
             return imageBytes;
-        } catch (IOException e) {
-            throw new FrameworkRuntimeException("Exception while taking screenshot.", e);
+
+        } catch (Exception e) {
+            saveTextLog(e.toString(), "Ошибка при снятии скриншота");
+            return new byte[]{};
         }
+    }
+
+
+    // 10 раз пытается переключиться на аллерт, так как не всегда срабатывает с 1 раза
+    private static boolean isAlertPresented(WebDriver driver) {
+        for (int i = 0; i < 10; i++) {
+            try {
+                driver.switchTo().alert();
+                return true;
+            } catch (Exception ignore) {
+            }
+        }
+        return false;
+    }
+
+    @Attachment(value = "{1}", type = "text/plain")
+    private static String saveTextLog(String message, String caption) {
+        return message;
     }
 }
