cdff0381b6482f2ab6a2c8d107be9c3610971f55
Muhametdinov <Muhametdinov@lanit.ru>
2018-12-18 16:47:36 +0400

    driver 3.0.14 fix

--- src/main/java/ru/lanit/at/pages/block_elements/AbstractBlockElement.java
+++ src/main/java/ru/lanit/at/pages/block_elements/AbstractBlockElement.java
@@ -6,9 +6,4 @@ import org.openqa.selenium.WrapsDriver;
 import ru.lanit.at.pages.FrameworkBaseElement;
 
 public interface AbstractBlockElement<T extends WebElement> extends AtlasWebElement<T>, FrameworkBaseElement, WrapsDriver {
-
-    @Override
-    default WebElement getWrappedElement(){
-        return this;
-    }
 }
