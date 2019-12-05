2b17699745824cbf2f007736e0444ea17613279a
sorokin_boris <BSorokin@lanit.ru>
2019-12-05 10:35:19 +0300

    1) add citrus runner 2) fixed bug of WrappedDriver

--- src/main/java/ru/lanit/at/pages/AbstractPage.java
+++ src/main/java/ru/lanit/at/pages/AbstractPage.java
@@ -1,7 +1,14 @@
 package ru.lanit.at.pages;
 
 import io.qameta.atlas.webdriver.WebPage;
+import org.openqa.selenium.WebDriver;
+import ru.lanit.at.context.Context;
+import ru.lanit.at.driver.DriverManager;
 
 public interface AbstractPage extends WebPage, SearchBlockElement {
 
+    @Override
+    default WebDriver getWrappedDriver() {
+        return Context.getInstance().getBean(DriverManager.class).getDriver();
+    }
 }
--- src/main/java/ru/lanit/at/steps/AbstractFrameworkSteps.java
+++ src/main/java/ru/lanit/at/steps/AbstractFrameworkSteps.java
@@ -1,10 +1,12 @@
 package ru.lanit.at.steps;
 
+import com.consol.citrus.message.Message;
 import io.qameta.atlas.core.Atlas;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.springframework.context.ApplicationContext;
+import ru.lanit.at.citrus.CitrusRunner;
 import ru.lanit.at.context.Context;
 import ru.lanit.at.driver.DriverManager;
 import ru.lanit.at.exceptions.FrameworkRuntimeException;
@@ -25,6 +27,7 @@ public abstract class AbstractFrameworkSteps {
     private ScreenShooter screenShooter;
     private DriverManager driverManager;
     private Atlas atlas;
+    private CitrusRunner citrusRunner;
 
 
     public AbstractFrameworkSteps() {
@@ -33,6 +36,7 @@ public abstract class AbstractFrameworkSteps {
         driverManager = context.getBean(DriverManager.class);
         atlas = pageCatalog.getAtlas();
         screenShooter = context.getBean(ScreenShooter.class);
+        citrusRunner = context.getBean(CitrusRunner.class);
     }
 
     /**
@@ -237,6 +241,7 @@ public abstract class AbstractFrameworkSteps {
         return (T) getDataKeeper().get(key);
     }
 
+
     /**
      * Erases all saved test data.
      */
@@ -265,5 +270,86 @@ public abstract class AbstractFrameworkSteps {
 
     public ScreenShooter getScreenShooter() {
         return screenShooter;
+    }
+
+
+    /**
+     * Returns an instance of CitrusRunner  {@link CitrusRunner}
+     *
+     * @return instance CitrusRunner
+     */
+    public CitrusRunner getCitrusRunner() {
+        return citrusRunner;
+    }
+
+    /**
+     * Returns an Object of Endpoint
+     *
+     * @param name - Endpoint Name (@link org.springframework.context.annotation.Bean#name())
+     * @return object of Endpoint
+     */
+    public Object getEndpointByName(String name) {
+        return citrusRunner.getTestContext().getApplicationContext().getBean(name);
+    }
+
+    /**
+     * Returns an Object of Endpoint
+     *
+     * @param requiredType type the bean must match; can be an interface or superclass
+     *                     return an instance of the single bean matching the required type
+     */
+    public <T> T getEndpointByClassName(Class<T> requiredType) {
+        return citrusRunner.getTestContext().getApplicationContext().getBean(requiredType);
+    }
+
+
+    public <T> void setCitrusVariable(String key, T value) {
+        citrusRunner.variable(key, value);
+    }
+
+
+    /**
+     * Gets the value for the given variable expression. Expression usually is the
+     * simple variable name, with optional expression prefix/suffix.
+     * <p>
+     * In case variable is not known to the context throw runtime exception.
+     *
+     * @param key expression to search for.
+     * @return value of the variable
+     */
+    public String getCitrusVariable(String key) {
+        return citrusRunner.getTestContext().getVariable(key);
+    }
+
+    /**
+     * Gets typed variable value.
+     *
+     * @param key  expression to search for.
+     * @param type Class type
+     * @return typed variable value
+     */
+    public <T> T getCitrusVariable(String key, Class<T> type) {
+        return citrusRunner.getTestContext().getVariable(key, type);
+    }
+
+    /**
+     * Gets the value for the given variable as object representation.
+     * Use this method if you seek for test objects stored in the context.
+     *
+     * @param variableExpression expression to search for.
+     * @return value of the variable as object
+     */
+    public Object getVariableObject(String variableExpression) {
+        return citrusRunner.getTestContext().getVariable(variableExpression);
+    }
+
+    /**
+     * Gets message by name.
+     *
+     * @param name message id.
+     * @return message Object
+     */
+    public Message getMessageByName(String name) {
+        return citrusRunner.getTestContext().getMessageStore().getMessage(name);
     }
 }
