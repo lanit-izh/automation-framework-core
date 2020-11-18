d57631394f1f61f9f2c16e748535da1aec95f087
Baldroster <arhar18rus@gmail.com>
2020-11-18 15:30:19 +0400

    [+]Swipes and scrolls extended

--- src/main/java/ru/lanit/at/steps/AbstractFrameworkAndroidSteps.java
+++ src/main/java/ru/lanit/at/steps/AbstractFrameworkAndroidSteps.java
@@ -4,9 +4,11 @@ import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.TouchAction;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
-import ru.lanit.at.Config;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
 import ru.lanit.at.pages.element.MobileUIElement;
 import ru.lanit.at.pages.element.UIElement;
+import java.util.concurrent.TimeUnit;
 
 import static io.appium.java_client.touch.offset.PointOption.point;
 import static ru.lanit.at.FrameworkConstants.*;
@@ -14,18 +16,18 @@ import static ru.lanit.at.FrameworkConstants.*;
 
 public class AbstractFrameworkAndroidSteps extends AbstractFrameworkSteps {
 
-    static private final int numberOfScrolls;
-    static {
-        numberOfScrolls =  new Config(DEFAULT_TIMEOUTS_CONFIG).getProperty(SCROLL_TIMES, 10);
+
+    protected int getNumberOfScrolls() {
+        return getDriverManager().getDriverTimeouts().getProperty(SCROLL_TIMES, 10);
     }
 
-    public int getNumberOfScrolls(){
-        return numberOfScrolls;
-
+    protected int getImplicitlyTimeout() {
+        return getDriverManager().getDriverTimeouts().getProperty(IMPLICITLY_WAIT, 10);
     }
+
     public MobileUIElement scrollToElement(Class<? extends UIElement> type, int numberOfTimes, String... params) {
         MobileUIElement element = null;
-
+        getDriver().manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
         for (int i = 0; i < numberOfTimes; i++) {
             try {
                 scroll(true);
@@ -36,19 +38,21 @@ public class AbstractFrameworkAndroidSteps extends AbstractFrameworkSteps {
                 System.out.println(String.format("Element not available. Scrolling (%s) times...", i + 1));
             }
         }
+        getDriver().manage().timeouts().implicitlyWait(getImplicitlyTimeout(), TimeUnit.SECONDS);
         return element;
 
 
     }
 
     public MobileUIElement scrollToElement(Class<? extends UIElement> type, String... params) {
-        return scrollToElement(type, numberOfScrolls, params);
+        return scrollToElement(type, getNumberOfScrolls(), params);
 
     }
 
     public MobileUIElement scrollToElementByName(Class<? extends UIElement> type, String elementName, int numberOfTimes) {
         MobileUIElement element = null;
 
+        getDriver().manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
         for (int i = 0; i < numberOfTimes; i++) {
             try {
                 scroll(true);
@@ -59,13 +63,66 @@ public class AbstractFrameworkAndroidSteps extends AbstractFrameworkSteps {
                 System.out.println(String.format("Element not available. Scrolling (%s) times...", i + 1));
             }
         }
+        getDriver().manage().timeouts().implicitlyWait(getImplicitlyTimeout(), TimeUnit.SECONDS);
         return element;
 
 
     }
 
     public MobileUIElement scrollToElementByName(Class<? extends UIElement> type, String elementName) {
-        return scrollToElementByName(type, elementName, numberOfScrolls);
+        return scrollToElementByName(type, elementName, getNumberOfScrolls());
+
+    }
+
+
+    public void swipeIntoElement(boolean isRight, WebElement element) {
+        int startX;
+        int endX;
+        int startY;
+        Dimension size = element.getSize();
+        Point position = element.getLocation();
+        if (!isRight) {
+            startY = (position.y + size.height / 2);
+            startX = (int) (position.x + size.width * 0.90);
+            endX = (int) (position.x + size.width * 0.05);
+        } else {
+            startY = (position.y + size.height / 2);
+            startX = (int) (position.x + size.width * 0.05);
+            endX = (int) (position.x + size.width * 0.90);
+        }
+        new TouchAction((PerformsTouchActions) getDriver())
+                .press(point(startX, startY))
+                .moveTo(point(endX, startY))
+                .release()
+                .perform();
+    }
+
+    public void swipe(boolean isRight) {
+        int startX;
+        int endX;
+        int startY;
+        Dimension size = getDriver().manage().window().getSize();
+        if (!isRight) {
+            startY = (size.height / 2);
+            startX = (int) (size.width * 0.90);
+            endX = (int) (size.width * 0.05);
+        } else {
+            startY = (size.height / 2);
+            startX = (int) (size.width * 0.05);
+            endX = (int) (size.width * 0.90);
+        }
+        new TouchAction((PerformsTouchActions) getDriver())
+                .press(point(startX, startY))
+                .moveTo(point(endX, startY))
+                .release()
+                .perform();
+    }
+
+
+    public void tap(int tapX, int tapY) {
+        Dimension size = getDriver().manage().window().getSize();
+        new TouchAction((PerformsTouchActions) getDriver())
+                .tap(point(tapX, tapY)).perform();
 
     }
 
@@ -77,13 +134,37 @@ public class AbstractFrameworkAndroidSteps extends AbstractFrameworkSteps {
         int endPoint;
         Dimension size = getDriver().manage().window().getSize();
         if (isDown) {
-            anchor = (int) (size.width / 2);
-            startPoint = (int) (size.height / 2);
+            anchor = size.width / 2;
+            startPoint = size.height / 2;
             endPoint = size.height / 8;
         } else {
-            anchor = (int) (size.width / 2);
+            anchor = size.width / 2;
             startPoint = size.height - (size.height / 8);
-            endPoint = (int) (size.height / 2);
+            endPoint = size.height / 2;
+        }
+        new TouchAction((PerformsTouchActions) getDriver())
+                .longPress(point(anchor, startPoint))
+                .moveTo(point(anchor, endPoint))
+                .release()
+                .perform();
+    }
+
+    public void scrollIntoElement(boolean isDown, WebElement element) {
+
+        int anchor;
+        int startPoint;
+        int endPoint;
+
+        Point position = element.getLocation();
+        Dimension size = element.getSize();
+        if (isDown) {
+            anchor = position.x + size.width / 2;
+            startPoint = position.y + size.height / 2;
+            endPoint = position.y + size.height / 16;
+        } else {
+            anchor = position.x + size.width / 2;
+            startPoint = position.y + size.height - (size.height / 16);
+            endPoint = position.y + size.height / 2;
         }
         new TouchAction((PerformsTouchActions) getDriver())
                 .longPress(point(anchor, startPoint))
